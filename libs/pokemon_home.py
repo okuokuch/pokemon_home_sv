4ef20677086b02d0c3b713c15bb1eb64e1ceebf1
okuokuch <okuokuch@gmail.com>
2023-02-21 22:40:22 +0900

    アウトプットに必要なメソッドの追加

--- libs/pokemon_home.py
+++ libs/pokemon_home.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import pandas as pd
+from tqdm import tqdm
 
 
 class pokemon_home:
@@ -16,9 +17,9 @@ class pokemon_home:
         """
         self.POKEMON = self.__read_json("{}/pokemon_names.json".format(folder_path))[language]
         self.MOVE = self.__read_json("{}/move_names.json".format(folder_path))[language]
-        self.NATURE = self.__read_json("{}/nature_names.json".format(folder_path))[language]
+        self.ABILITY = self.__read_json("{}/ability_names.json".format(folder_path))[language]
         self.TYPE = self.__read_json("{}/type_names.json".format(folder_path))[language]
-        self.ITEM = self.__read_json("{}/ability_names.json".format(folder_path))
+        self.ITEM = self.__read_json("{}/item_names.json".format(folder_path))["itemname"]
 
     def __read_json(self, path: str):
         """jsonファイルをutf-8で読み込む"""
@@ -74,18 +75,92 @@ class pokemon_home:
         response = requests.get(url.format(cid=self.params["cid"], rst=self.params["rst"], ts=self.params["ts2"]))
         return json.loads(response.text)
 
+    def __fetch_pokemon_detail(self, num: int):
+        url = "https://resource.pokemon-home.com/battledata/ranking/scvi/{cid}/{rst}/{ts}/pdetail-{num}"
+        response = requests.get(
+            url.format(cid=self.params["cid"], rst=self.params["rst"], ts=self.params["ts2"], num=num)
+        )
+        return json.loads(response.text)
+
+    def __convert_id_to_name(self, id: str | int, mapping_data: list | dict):
+        if type(mapping_data) == list:
+            name = mapping_data[int(id)]
+        elif type(mapping_data) == dict:
+            name = mapping_data[str(id)]
+        return name
+
+    def __parse_pokemon_detail(self, detail_json: dict):
+        output_move = []
+        output_ability = []
+        output_item = []
+        output_type = []
+        for pokemon_id, value in tqdm(detail_json.items()):
+            pokemon_name = self.__convert_id_to_name(int(pokemon_id) - 1, self.POKEMON)
+            for form_id, value_2 in value.items():
+                pokemon_info = value_2["temoti"]
+                output_move.extend(
+                    self.__output_detail(pokemon_info["waza"], pokemon_name, pokemon_id, form_id, self.MOVE)
+                )
+                output_ability.extend(
+                    self.__output_detail(pokemon_info["tokusei"], pokemon_name, pokemon_id, form_id, self.ABILITY)
+                )
+                output_item.extend(
+                    self.__output_detail(pokemon_info["motimono"], pokemon_name, pokemon_id, form_id, self.ITEM)
+                )
+                output_type.extend(
+                    self.__output_detail(pokemon_info["terastal"], pokemon_name, pokemon_id, form_id, self.TYPE)
+                )
+        return output_move, output_ability, output_item, output_type
+
+    def __output_detail(self, info_json: list, pokemon_name: str, pokemon_id: str, form_id: str, mapping_data: dict):
+        output = []
+        for i, info in enumerate(info_json):
+            name = self.__convert_id_to_name(info["id"], mapping_data)
+            output.append([pokemon_name, pokemon_id, form_id, i + 1, name, info["val"]])
+        return output
+
     def output_pokemon_ranking(self):
         pokemon_ranking = self.__fetch_pokemon_ranking()
         output = []
         for i, pokemon in enumerate(pokemon_ranking):
-            pokemon_name = self.POKEMON[pokemon["id"] - 1]
+            pokemon_name = self.__convert_id_to_name(pokemon["id"] - 1, self.POKEMON)
             form_id = pokemon["form"]
             data = [i + 1, pokemon_name, form_id]
             output.append(data)
         return output
 
+    def output_pokemon_detail(self):
+        output_move = []
+        output_ability = []
+        output_item = []
+        output_type = []
+        for i in range(1, 6):
+            pokemon_all_detail = self.__fetch_pokemon_detail(i)
+            move, ability, item, terastype = self.__parse_pokemon_detail(pokemon_all_detail)
+            output_move.extend(move)
+            output_ability.extend(ability)
+            output_item.extend(item)
+            output_type.extend(terastype)
+        return output_move, output_ability, output_item, output_type
+
 
 if __name__ == "__main__":
     pokemon = pokemon_home("./asset")
     pokemon.request_parameters_from_season_info(3, 0)
-    print(pd.DataFrame(pokemon.output_pokemon_ranking()))
+    move, ability, item, terastype = pokemon.output_pokemon_detail()
+    pd.DataFrame(move, columns=["pokemon", "id", "form", "rank", "move", "raito"]).to_csv(
+        "./output/move.csv",
+        encoding="shift-jis",
+    )
+    pd.DataFrame(ability, columns=["pokemon", "id", "form", "rank", "ability", "raito"]).to_csv(
+        "./output/ability.csv",
+        encoding="shift-jis",
+    )
+    pd.DataFrame(item, columns=["pokemon", "id", "form", "rank", "item", "raito"]).to_csv(
+        "./output/item.csv",
+        encoding="shift-jis",
+    )
+    pd.DataFrame(terastype, columns=["pokemon", "id", "form", "rank", "tarastype", "raito"]).to_csv(
+        "./output/terastype.csv",
+        encoding="shift-jis",
+    )
